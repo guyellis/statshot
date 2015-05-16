d725541c234fe6d5678e2de497b870823b8051f9
Guy Ellis <wildfiction@gmail.com>
2015-05-15 19:03:37 -0700

    Add test for help

---
+++ test/lib/test.help.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// var _ = require('lodash');
+var chai = require('chai');
+var help = require('../../lib/help');
+var sinon = require('sinon');
+
+chai.should();
+
+describe('Help', function(){
+
+  it('should print a help message', function(done){
+    var stub = sinon.stub(console, 'log', function(message){
+      message.should.contain('--help');
+      stub.restore();
+      done();
+    });
+    help();
+  });
+
+});
